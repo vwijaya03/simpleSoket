ff4577f50f0a0e267c5e6c570457a96ddc6ce87d
AXEtronot <vikowijaya@rock1.foamingrocks.com>
2015-10-27 09:59:38 +0700

    asdasd

---
+++ src/simpleController.php
@@ -0,0 +1,13 @@
+<?php namespace simple\soket;
+
+use App\Models\Message;
+use App\Http\Controllers\Controller;
+use \Input;
+
+class MessageController extends Controller {
+
+	public function index()
+	{
+		
+	}
+}
